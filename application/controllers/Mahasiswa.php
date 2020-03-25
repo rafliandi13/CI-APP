1c8775863c832fec12203ac3dff3dee61cbd8702
unknown <herp1306@gmail.com>
2020-03-25 19:00:03 +0700

    initial commit

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,68 @@
+<?php
+
+class Mahasiswa extends CI_Controller {
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+        $this->load->library('form_validation');
+    }
+    public function tambah(){
+        $data['judul'] = 'Form Tambah Data Mahasiswa';
+        $data['jurusan'] = ['Teknik Informatika', 'Teknik Mesin', 'Teknik Elektro', 'Teknik Kimia','Teknik Sipil'];
+        $this->form_validation->set_rules('nama','Nama','required');
+        $this->form_validation->set_rules('nim','Nim','required|numeric');
+        $this->form_validation->set_rules('email','Email','required|valid_email');
+        if($this->form_validation->run() == FALSE){
+        $this->load->view('templates/header', $data);
+        $this->load->view('mahasiswa/tambah',$data);
+        $this->load->view('templates/footer');
+        }else{
+            $this->Mahasiswa_model->tambahDataMahasiswa();
+            $this->session->set_flashdata('flash','Ditambahkan');
+            redirect('mahasiswa');
+        }
+    }
+    public function index(){
+        
+        $data['judul'] = 'Daftar Mahasiswa'; 
+        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+        if($this->input->post('keyword')){
+            $data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
+        }
+        $this->load->view('templates/header.php',$data);
+        $this->load->view('mahasiswa/index',$data);
+        $this->load->view('templates/footer.php');
+        
+    }
+    public function hapus($id){
+        $this->Mahasiswa_model->hapusDataMahasiswa($id);
+        $this->session->set_flashdata('flash','Dihapus');
+        redirect('mahasiswa');
+    }
+    public function detail($id){
+        $data['judul'] = 'Detail Data Mahasiswa';
+        $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+        $this->load->view('templates/header',$data);
+        $this->load->view('mahasiswa/detail',$data);
+        $this->load->view('templates/footer');
+    }
+    public function ubah($id){
+        $data['judul'] = 'Form Ubah Data Mahasiswa';
+        $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+        $data['jurusan'] = ['Teknik Informatika', 'Teknik Mesin', 'Teknik Elektro', 'Teknik Kimia','Teknik Sipil'];
+        
+        $this->form_validation->set_rules('nama','Nama','required');
+        $this->form_validation->set_rules('nim','Nim','required|numeric');
+        $this->form_validation->set_rules('email','Email','required|valid_email');
+        
+        if($this->form_validation->run() == FALSE){
+            $this->load->view('templates/header', $data);
+            $this->load->view('mahasiswa/ubah',$data);
+            $this->load->view('templates/footer');
+        }else{
+            $this->Mahasiswa_model->ubahDataMahasiswa();
+            $this->session->set_flashdata('flash','DiUbah');
+            redirect('mahasiswa');
+        }
+    }
+}
---
+++ application/views/Home/index.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+  <h1 class="display-4">Selamat Datang di Website CRUD menggunakan CodeIgniter</h1>
+  <p class="lead"></p>
+  <hr class="my-4">
+  <p>Hi, <?= $nama; ?></p>
+</div>
